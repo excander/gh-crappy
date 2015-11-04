80d611fe145e72b2567e93aacc8c6eb59aa57799
excander <excander@mail.ru>
2015-11-04 23:21:16 +0300

    changed \ to / in parse/views.py

--- parse/views.py
+++ parse/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 
 def parse_view(request): 
-    return render(request, 'parse\parse_view.html')
+    return render(request, 'parse/parse_view.html')
