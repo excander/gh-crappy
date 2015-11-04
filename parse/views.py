9cde2d73bef6019ed201f46222cd881f67805f66
excander <excander@mail.ru>
2015-11-05 01:44:53 +0300

    my own first crappy view: creates res.html with range

--- parse/views.py
+++ parse/views.py
@@ -11,7 +11,16 @@ def parse_view(request):
         form = ParseForm(request.POST)
         if form.is_valid():
             infield = form.cleaned_data['infield']
-            return redirect('parse_result', infield=infield)
+            if infield.isdigit():
+                f = open("parse/templates/parse/res.html", 'w')
+                f.write('''{% extends "parse/base.html" %} {% block content %}''')
+                for i in range(int(infield)):
+                    f.write("<p>"+str(i)+"</p>")
+                f.write("{% endblock content %}")
+                f.close()
+                return render(request, 'parse/res.html')
+            else:
+                return redirect('parse_result', infield=infield)
     else:
         form = ParseForm()
     return render(request, 'parse/parse_view.html', {'form': form})
